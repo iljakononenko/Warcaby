657007a5cf930b59c5570e8701c1af953831bece
iljakononenko <iljakononenko3@gmail.com>
2021-01-11 08:31:33 +0100

    Online version (sockets with javafx (not working)) Still TODO: - Fix some moving - Add jumping - Add Victory check

--- src/main/java/com/iljaknk/App.java
+++ src/main/java/com/iljaknk/App.java
@@ -1,21 +1,30 @@
 package com.iljaknk;
 
+import com.iljaknk.Game.Game_engine;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+import static com.iljaknk.Game.Game_engine.list_of_players;
 
 public class App extends Application {
+
     public static int players_turn = 0;
     public static Label players_turn_label = new Label();
+    public static Label label_Players_connected = new Label();
     public static Stage window;
     public static Scene menu_scene, game_scene;
     public static int number_of_players = 6;
@@ -24,13 +33,19 @@ public class App extends Application {
     public static Group node_Group = new Group();
     public static Group piece_Group = new Group();
 
+    static Socket socket;
+    static Scanner in;
+    static PrintWriter out;
+    public Button end_turn_button;
+    public Button start_game_button;
+
     @Override
     public void start(Stage stage) throws IOException
     {
+
         players_turn_label.setAlignment(Pos.CENTER);
 
         players_turn_label.setFont(Font.font(24));
-
 
         window = stage;
 
@@ -50,13 +65,68 @@ public class App extends Application {
 
         window.setScene(menu_scene);
         window.show();
+        //run();
     }
 
-    public static void main(String[] args) {
+    // menu window controller
+
+    public void start_game()
+    {
+        Game_engine game_engine = new Game_engine();
+        game_engine.start_Game();
+        window.setScene(game_scene);
+        out.println("START_GAME");
+    }
+
+    // game_window controller part
+
+    public void end_turn ()
+    {
+        Game_engine.end_turn();
+        change_turn_label();
+        out.println("NEXT_TURN");
+    }
+
+    public void change_turn_label ()
+    {
+        players_turn_label.setText("Player " + (players_turn + 1) + " turn! (" + list_of_players.get_player_color(players_turn) + ")");
+    }
+
+    // run method for server
+
+    public static void run()
+    {
+        try
+        {
+            socket = new Socket("localhost", 56521);
+
+            in = new Scanner(socket.getInputStream());
+
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+            while (in.hasNextLine())
+            {
+                var line = in.nextLine();
+                System.out.println(line);
+                if (line.startsWith("ONLINE_UPDATE"))
+                {
+                    label_Players_connected.setText(line.substring(14) + " / 6 Players are connected");
+                }
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            window.close();
+        }
+    }
+
+    public static void main(String[] args)
+    {
         launch();
+
     }
-
-
-
-
 }
