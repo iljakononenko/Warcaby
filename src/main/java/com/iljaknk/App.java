772d3b8977172e4fa26b3cf50d48f70f407149f6
iljakononenko <iljakononenko3@gmail.com>
2021-01-12 18:58:16 +0100

    Online version (sockets with javafx (not working)) Still TODO: - Fix some moving - Add jumping - Add Victory check

--- src/main/java/com/iljaknk/App.java
+++ src/main/java/com/iljaknk/App.java
@@ -1,12 +1,19 @@
 package com.iljaknk;
 
+import com.iljaknk.Client_Server.Network_Connection;
+import com.iljaknk.Client_Server.Online_Player;
+import com.iljaknk.Client_Server.Server;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -14,6 +21,14 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class App extends Application {
+
+    private static boolean is_Server = false;
+
+    public static TextArea messages_output = new TextArea();
+    public static TextField messages_input = new TextField();
+
+    public static Network_Connection connection = is_Server ? create_Server() : create_Player();
+
     public static int players_turn = 0;
     public static Label players_turn_label = new Label();
     public static Stage window;
@@ -25,12 +40,40 @@ public class App extends Application {
     public static Group piece_Group = new Group();
 
     @Override
+    public void init() throws Exception
+    {
+        connection.start_Connection();
+    }
+
+    @Override
     public void start(Stage stage) throws IOException
     {
+        messages_input.setOnAction(event ->
+        {
+            String sms = is_Server ? "Server: " : "Player: ";
+            sms += messages_input.getText();
+            messages_input.clear();
+
+            messages_output.appendText(sms + "\n");
+
+            try
+            {
+                connection.send(sms);
+            }
+            catch (Exception e)
+            {
+                messages_output.appendText("Failed to send");
+            }
+        });
+        messages_output.setPrefHeight(150);
+        messages_output.setPrefWidth(200);
+        VBox chat_panel = new VBox(20, messages_output, messages_input);
+        chat_panel.setPrefSize(200, 200);
+
+
         players_turn_label.setAlignment(Pos.CENTER);
 
         players_turn_label.setFont(Font.font(24));
-
 
         window = stage;
 
@@ -43,7 +86,9 @@ public class App extends Application {
         BorderPane.setAlignment(players_turn_label, Pos.TOP_CENTER);
         parent_game_pane.setCenter(game_pane);
 
-        Parent menu = FXMLLoader.load(App.class.getResource("/menu_window.fxml"));
+        BorderPane menu = FXMLLoader.load(App.class.getResource("/menu_window.fxml"));
+
+        menu.setLeft(chat_panel);
 
         menu_scene = new Scene(menu);
         game_scene = new Scene(parent_game_pane);
@@ -52,11 +97,36 @@ public class App extends Application {
         window.show();
     }
 
+    @Override
+    public void stop () throws Exception
+    {
+        connection.close_Connection();
+    }
+
+    private static Server create_Server()
+    {
+        return new Server(55555, data ->
+        {
+            Platform.runLater(() ->
+            {
+                messages_output.appendText(data.toString() + "\n");
+            });
+        });
+    }
+
+    private static Online_Player create_Player()
+    {
+        return new Online_Player("localhost", 55555, data ->
+        {
+            Platform.runLater(() ->
+            {
+                messages_output.appendText(data.toString() + "\n");
+            });
+        });
+    }
+
     public static void main(String[] args) {
         launch();
     }
-
-
-
 
 }
--- src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-module com.iljaknk {
-    requires javafx.controls;
-    requires javafx.fxml;
-    exports com.iljaknk;
-}
